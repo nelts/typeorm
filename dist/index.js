19b4113f6a99520b04c5a3344cddbfe90012a88d
evio <evio@vip.qq.com>
2019-08-29 19:16:01 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("./typeorm");
 exports.TypeOrm = typeorm_1.default;
-function AutoBindWorkerORM(plu, configs, Tables) {
+function AutoBindWorkerORM(name, plu, configs, Tables) {
     const app = plu.app;
     const ormComponent = plu.getComponent('@nelts/typeorm');
-    const id = ormComponent.typeorm.preset(configs.mysql, Tables);
+    const id = ormComponent.typeorm.preset(configs, Tables);
     const conn = ormComponent.typeorm.get(id);
-    app.on('ContextStop', async (ctx) => await ctx.mysql.release());
+    app.on('ContextStop', async (ctx) => ctx[name] && await ctx[name].release());
     app.on('ContextStart', async (ctx) => {
-        ctx.mysql = conn.createQueryRunner();
-        await ctx.mysql.connect();
-        ctx.mysql.begin = async () => {
-            await ctx.mysql.startTransaction();
-            ctx.on('ContextResolve', async () => await ctx.mysql.commitTransaction());
-            ctx.on('ContextReject', async () => await ctx.mysql.rollbackTransaction());
+        ctx[name] = conn.createQueryRunner();
+        await ctx[name].connect();
+        ctx[name].begin = async () => {
+            await ctx[name].startTransaction();
+            ctx.on('ContextResolve', async () => await ctx[name].commitTransaction());
+            ctx.on('ContextReject', async () => await ctx[name].rollbackTransaction());
         };
     });
 }
