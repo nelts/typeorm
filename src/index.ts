b1a101d48b4a31d8ac81759b3c91cfb7990e0853
evio <evio@vip.qq.com>
2019-08-29 17:46:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,12 @@ import AgentFactory, { AgentPlugin } from '@nelts/agent';
 import MasterFactory, { MasterPlugin } from '@nelts/master';
 import TypeOrm from './typeorm';
 import { EntitySchema, QueryRunner } from 'typeorm';
-import { EventEmitter } from '@nelts/utils';
 
 export interface IQueryRunner extends QueryRunner {
   begin?(): Promise<any>;
 }
 
 // worker interfaces
-export interface LocalWorkerContext extends EventEmitter {
-  mysql: IQueryRunner;
-}
 export interface LocalWorkerPlugin<T extends WorkerServiceFrameworker> extends WorkerPlugin<T> {
   typeorm: TypeOrm,
 }
@@ -44,22 +40,23 @@ export {
 }
 
 export function AutoBindWorkerORM<T extends LocalWorkerPlugin<U>, U extends WorkerServiceFrameworker>(
+  name: string,
   plu: T, 
   configs: any, 
   Tables: (Function | string | EntitySchema<any>)[]
 ) {
   const app = plu.app as LocalWorkerFactory<U>;
   const ormComponent = plu.getComponent<LocalWorkerPlugin<U>>('@nelts/typeorm');
-  const id = ormComponent.typeorm.preset(configs.mysql, Tables);
+  const id = ormComponent.typeorm.preset(configs, Tables);
   const conn = ormComponent.typeorm.get(id);
-  app.on('ContextStop', async (ctx: LocalWorkerContext) => await ctx.mysql.release());
-  app.on('ContextStart', async (ctx: LocalWorkerContext) => {
-    ctx.mysql = conn.createQueryRunner();
-    await ctx.mysql.connect();
-    ctx.mysql.begin = async () => {
-      await ctx.mysql.startTransaction();
-      ctx.on('ContextResolve', async () => await ctx.mysql.commitTransaction());
-      ctx.on('ContextReject', async () => await ctx.mysql.rollbackTransaction());
+  app.on('ContextStop', async (ctx: any) => ctx[name] && await ctx[name].release());
+  app.on('ContextStart', async (ctx: any) => {
+    ctx[name] = conn.createQueryRunner();
+    await ctx[name].connect();
+    ctx[name].begin = async () => {
+      await ctx[name].startTransaction();
+      ctx.on('ContextResolve', async () => await ctx[name].commitTransaction());
+      ctx.on('ContextReject', async () => await ctx[name].rollbackTransaction());
     }
   });
 }
